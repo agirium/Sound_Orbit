5b4e7fa37dde48c0d7f8a802728c28cc91ca8635
agirium <110819276+agirium@users.noreply.github.com>
2025-07-14 16:16:43 +0900

    Update server.js

--- server.js
+++ server.js
@@ -4,55 +4,45 @@ const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 app.use(express.static("public"));
 
-let dots = {}; // { id: { r, theta, color, lastActive } }
+let dots = {}; // { id: { x, y, r, theta, color } }
 
-// 接続時
+function getRandomColor() {
+  const hue = Math.floor(Math.random() * 360);
+  return `hsl(${hue}, 100%, 80%)`;
+}
+
 io.on("connection", (socket) => {
-  // 初期化
-  dots[socket.id] = { 
-    r: 0, 
-    theta: 0, 
-    color: "#00aaff",
-    lastActive: Date.now()
+  // 接続時にランダムな色を割り当てる
+  dots[socket.id] = {
+    x: 0,
+    y: 0,
+    r: 0,
+    theta: 0,
+    color: getRandomColor()
   };
 
-  // 色の受信
-  socket.on("color", (color) => {
-    if (dots[socket.id]) {
-      dots[socket.id].color = color;
-      dots[socket.id].lastActive = Date.now();
-    }
-  });
+  // 自分のIDを送信
+  socket.emit("init", socket.id);
 
-  // 位置の受信
+
+
+  // 位置情報更新
   socket.on("move", (pos) => {
     if (dots[socket.id]) {
       dots[socket.id].r = pos.r;
       dots[socket.id].theta = pos.theta;
-      dots[socket.id].lastActive = Date.now();
     }
   });
 
-  // 切断時
   socket.on("disconnect", () => {
     delete dots[socket.id];
   });
+
+  // 全クライアントに送信
+  setInterval(() => {
+    io.emit("updateAll", dots);
+  }, 50);
 });
-
-// 定期的に古いユーザーを削除
-setInterval(() => {
-  const now = Date.now();
-  for (const id in dots) {
-    if (now - dots[id].lastActive > 5000) { // 5秒以上更新なし
-      delete dots[id];
-    }
-  }
-}, 1000);
-
-// 全体の状態をブロードキャスト
-setInterval(() => {
-  io.emit("updateAll", dots);
-}, 50);
 
 http.listen(3000, () => {
   console.log("listening on http://localhost:3000");
